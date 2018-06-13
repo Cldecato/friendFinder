8c6473dcd20b82ff93ccb6f338dc57388e4606e1
corey decato <corey.decato@richmond.edu>
2018-06-12 20:09:24 -0400

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+var friendsData = [{
+    name: 'Test 1',
+    photo: 'https://www.thedailybeast.com/family-guy-reveals-shocking-truth-behind-stewies-accent-and-his-sexuality',
+    scores: ['1','1','1','1','1','1','1','1','1','1']
+},{
+    name: 'Test 2',
+    photo: 'https://www.thedailybeast.com/family-guy-reveals-shocking-truth-behind-stewies-accent-and-his-sexuality',
+    scores: ['5','5','5','5','5','5','5','5','5','5']
+},{
+    name: 'Test 3',
+    photo: 'https://www.thedailybeast.com/family-guy-reveals-shocking-truth-behind-stewies-accent-and-his-sexuality',
+    scores: ['3','3','3','3','3','3','3','3','3','3']
+}];
+
+module.exports = friendsData;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,37 @@
+const friendsData = require("../data/friends");
+
+module.exports = (app) => {
+    // API GET Requests
+    // Displays the JSON data of the friends database
+
+    app.get("/api/friends", (req, res) => {
+        res.json(friendsData);
+    });
+
+    app.post("/api/friends", (req, res) => {
+        // When the form is submited the data gets pushed to the friendsData array
+        console.log(req.body.scores);
+        var newFriend = req.body;
+        var masterCounter = [( 4 * newFriend.scores.length), 0];
+        for ( i = 0; i < friendsData.length; i++) {
+            var counter = 0;
+            for ( j = 0; j < friendsData[i].scores.length; j++) {
+                counter =+ Math.abs(newFriend.scores[j] - friendsData[i].scores[j]);
+            }
+            if (masterCounter[0] > counter) {
+                masterCounter[0] = counter;
+                masterCounter[1] = i;
+            }
+        }
+        // console.log(masterCounter);
+        console.log(friendsData[masterCounter[1]]);
+        friendsData.push(req.body);
+        res.json(friendsData[masterCounter[1]]);
+    });
+
+    app.post("/api/clear", () => {
+        // Empty out the arrays of data for testing
+        friendsData = [];
+        console.log(friendsData);
+    });
+};
